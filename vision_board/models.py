a0029fb28fe62b7c56fca42d682818fa659209c3
Trevor <trevodhiambo@gmail.com>
2022-04-15 06:40:58 +0300

    Created database model

--- vision_board/models.py
+++ vision_board/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Vision(models.Model):
+    title = models.CharField(max_length = 200)
+    description = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateField()
+    author = models.ForeignKey(get_user_model(),on_delete=models.CASCADE,)
+
+    def _str__(self):
+        return self.title
+    
+
+class Goal(models.Model):
+    vision = models.ForeignKey(Vision, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+class Intention(models.Model):
+    goal = models.ForeignKey(Goal, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
